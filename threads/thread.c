fc02a5fab78a3a12bf2707171062bc9dc164e636
starmpcc <kjune0322@naver.com>
2020-04-29 23:43:14 -0700

    Extended open file limit infinite, dup2 init
    Changed open_file structure
    Add dup2 init
    pass dup2_simple

--- threads/thread.c
+++ threads/thread.c
@@ -645,6 +645,7 @@ init_thread (struct thread *t, const char *name, int priority) {
 	t->parent = NULL;
 
 	t->magic = THREAD_MAGIC;
+	list_init (&t->open_file);
 
 	//for advanced scheduler
 	if (thread_mlfqs){
