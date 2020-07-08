35c5c7d43c409b02adaf050c45bd9e850c0ea955
Chanil Kim <chanil@kaist.ac.kr>
2020-07-09 02:15:40 +0900

    Remove duplicate dir_readdir call

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -814,8 +814,6 @@ mkdir_s (const char* name){
 static bool
 readdir_s (int fd, char* name){
 	struct thread_file* tf = get_tf (fd);
-	dir_readdir(tf->dir, name);
-	dir_readdir(tf->dir, name);
 	return dir_readdir(tf->dir, name);
 }
 
